bbee96d5aff5f1af5acef9e3a41782fda5dc830e
Rafael T Santos <texera09@gmail.com>
2025-11-24 21:01:12 -0300

    adiciona arquivo desafio_dia_11.py

---
+++ Desafio Comunidade DevCompleto/desafio_dia_11.py
@@ -0,0 +1,10 @@
+# Desafio 21 dias - Comunidade dev completo
+# Dia 11 - missão: 
+# Explorar entrada e saída de dados.
+# Criar um script que leia dados do usuário e mostre um relatório ou mensagem dinâmica.
+
+print("-" *20)
+print("Olá bem vindo!")
+print("-" *20)
+
+nome = input("Qual é o seu nome?")
