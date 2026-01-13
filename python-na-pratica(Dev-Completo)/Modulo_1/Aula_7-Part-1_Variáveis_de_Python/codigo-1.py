b7feb1cf5beb6f159f64936e126d6b4b5383c82b
Rafael T Santos <texera09@gmail.com>
2026-01-13 11:20:00 -0300

    Adiciona diretorio Aula_7-Part-2 e arquivo txt exercicios

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_7-Part-1_Variáveis_de_Python/codigo-1.py
@@ -0,0 +1,14 @@
+# minha_variavel = 10
+# print(minha_variavel)
+
+# minha_variavel = 20
+# print(minha_variavel)
+
+# nome = "Rafael"
+# print("Bem-vindo", nome)
+# print("Bem-vindo " + nome)
+# print(f"Bem-vindo {nome}")
+
+MAIOR_IDADE = 18  # Constante um valor que não será alterado no código, SEMPRE EM CAIXA ALTA.
+print(f"Maior idade atualmente: {MAIOR_IDADE}")
+
