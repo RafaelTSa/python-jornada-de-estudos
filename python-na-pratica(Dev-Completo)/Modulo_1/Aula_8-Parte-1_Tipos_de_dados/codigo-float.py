6571e8614fff6b1ceb7d015ce4e9caace56a10c6
Rafael T Santos <texera09@gmail.com>
2026-01-18 22:46:18 -0300

    adiciona arquivo codigo-float.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_8-Parte-1_Tipos_de_dados/codigo-float.py
@@ -0,0 +1,7 @@
+print(type(200.50))  # mesmo sem declarar uma variavel o python reconhece que o valor e do tipo "float" (possui casas decimais)
+
+valor_decimal = 100.20 # No python caso o ultimo valor seja "0" ele não é mostrado como uma forma de editar o valor total.
+print(valor_decimal)
+
+valor_decimal2 = 100.202
+print(valor_decimal2)
