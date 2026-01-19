b100e6e46df5141fe326c51056242e6f1b5c7d4e
Rafael T Santos <texera09@gmail.com>
2026-01-19 20:21:27 -0300

    adiciona exercicio2.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_8-Parte-2_exercicio_de_fixacao/exercicio2.py
@@ -0,0 +1,7 @@
+nome = "Rafael"
+idade = 39
+altura = 1.70
+gosta_de_programar = True
+
+print(f"O nome dele é {nome}, ele tem {idade} anos, ele tem {altura}mt e se ele gosta de programar: {gosta_de_programar}")
+
