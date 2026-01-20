e1a113c0ee066efb5dfda8aa920253a0734a8c91
Rafael T Santos <texera09@gmail.com>
2026-01-20 13:06:30 -0300

    adiciona arquivo aritmeticos.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_9-Part-1_Operadores_de_Python/aritmeticos.py
@@ -0,0 +1,13 @@
+x = 10
+y = 3
+
+print(x + y)  # mostra a soma " + " entre as variáveis.
+print(x - y)  # mostra a subtração " - " entre as variáveis.
+print(x * y)  # mostra a multiplicação " * "entre as variáveis.
+print(x / y)  # mostra a divisão " / " entre as variáveis.
+
+print(x // y)  # mostra a divisão inteira " // " entre as variáveis (sem mostrar a parte decimal).
+
+print(x ** y) # mostra a exponenciação " Xº "entre as variáveis (com o x sendo a base e o y sendo o expoente).
+
+print(x % y) # mostra apenas o resto da divisão " % " entre as variáveis.
