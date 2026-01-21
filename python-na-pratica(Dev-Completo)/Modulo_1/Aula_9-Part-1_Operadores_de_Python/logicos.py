758d07511d18e01f83be36a6a2865ebb60d3db4f
Rafael T Santos <texera09@gmail.com>
2026-01-21 19:37:23 -0300

    adicona logicos.py a Aula_9-Part-1

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_9-Part-1_Operadores_de_Python/logicos.py
@@ -0,0 +1,7 @@
+a = True
+b = False
+
+print(a and b) # operador de comparação logico and " E "
+print(a or b)  # operador de comparação logico or " OU "
+print(not a)   # operador de comparação logico not " NÂO "
+print(not b)
