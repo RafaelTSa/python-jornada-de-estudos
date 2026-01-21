17fd7247d627815f56c759cc5e6e9496ab4a7f9d
Rafael T Santos <texera09@gmail.com>
2026-01-20 23:16:56 -0300

    adiciona arquivo decimal.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_9-Part-1_Operadores_de_Python/decimal.py
@@ -0,0 +1,7 @@
+a = 0.1
+b = 0.2
+
+resultado = a + b
+
+print(a + b)
+print(round(resultado, 1)) # função "round" para arrendondar o valor da soma entre os números decimais e mostrar apenas 1 casa após a vírgula.
