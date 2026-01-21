5a0a11502c2601e54b2e92b0d217648d44e1ebc3
Rafael T Santos <texera09@gmail.com>
2026-01-21 06:28:05 -0300

    adiciona arquivo relacionais.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_9-Part-1_Operadores_de_Python/relacionais.py
@@ -0,0 +1,9 @@
+a = 10
+b = 11
+
+print(a == b)  # compara se a é "IGUAL" ao b com operador de comparação ( == )
+print(a != b)  # compara se a é "DIFERENTE" de b com operador de comparação ( != )
+print(a > b)   # compara se a é "MAIOR" ao b com operador de comparação ( > )
+print(a < b)   # compara se a é "MENOR" ao b com operador de comparação ( < )
+print(a >= b)  # compara se a é "MAIOR OU IGUAL" ao b com operador de comparação ( >= )
+print(a <= b) # compara se a é "MENOR OU IGUAL" ao b com operador de comparação ( <= )
