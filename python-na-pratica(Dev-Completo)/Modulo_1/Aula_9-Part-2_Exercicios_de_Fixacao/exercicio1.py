b5c8d8ac820d65a2a93a6b3965fc19f32caec532
Rafael T Santos <texera09@gmail.com>
2026-01-21 21:05:41 -0300

    adiciona exercicio1.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_9-Part-2_Exercicios_de_Fixacao/exercicio1.py
@@ -0,0 +1,19 @@
+a = 10
+b = 3
+
+soma = (a + b)
+subtracao = (a - b)
+multiplicacao = (a * b)
+divisao = (a / b)
+divisao_inteira = (a // b)
+expoente = (a ** b)
+resto_divisao = (a % b)
+
+print(f"Resultado da soma: {soma}")
+print(f"Resultado da subtração: {subtracao}")
+print(f"Resultado da multiplicação: {multiplicacao}")
+print(f"Resultado da divisão: {divisao}")
+print(f"Resultado da divisão inteira: {divisao_inteira}")
+print(f"Resultado da exponenciação: {expoente}")
+print(f"Resultado do resto da divisão: {resto_divisao}")
+
