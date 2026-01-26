ef65e9533e01f06e7103836c907629333a37ab2d
Rafael T Santos <texera09@gmail.com>
2026-01-26 18:04:51 -0300

    adiciona exercicio3.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_9-Part-2_Exercicios_de_Fixacao/exercicio3.py
@@ -0,0 +1,9 @@
+a = 5
+b = 9
+
+print(f"O valor de {a} e igual a {b}: {a == b}")
+print(f"O valor de {a} e maior que {b}: {a > b}")
+print(f"O valor de {a} e menor que {b}: {a < b}")
+print(f"O valor de {a} e menor ou igual a {b}: {a <= b}")
+print(f"O valor de {a} e maior ou igual a {b}: {a >= b}")
+print(f"O valor de {a} e diferente de {b}: {a != b}")
