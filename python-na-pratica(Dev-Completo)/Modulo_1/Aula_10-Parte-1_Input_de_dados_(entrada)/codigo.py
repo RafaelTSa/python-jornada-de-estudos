21324cb834436579eee335cb31737892c10332ea
Rafael T Santos <texera09@gmail.com>
2026-01-27 22:06:52 -0300

    adiciona exemplo de input e conversão de tipos

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_10-Parte-1_Input_de_dados_(entrada)/codigo.py
@@ -0,0 +1,7 @@
+nome = input("Digite o seu nome: ")
+print(f"Bem vindo {nome}!")
+print(type(nome))
+
+numero = int(input("Olá! Informe um número: "))
+print(f"O número que você digitou foi: {numero}")
+print(type(numero))
