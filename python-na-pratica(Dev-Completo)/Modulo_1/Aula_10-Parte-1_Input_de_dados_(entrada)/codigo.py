c65c76f0fddccc1c58073f487e10c861f5a86464
Rafael T Santos <texera09@gmail.com>
2026-03-05 10:38:33 -0300

    adiciona variaveis ao codigo.py

--- python-na-pratica(Dev-Completo)/Modulo_1/Aula_10-Parte-1_Input_de_dados_(entrada)/codigo.py
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_10-Parte-1_Input_de_dados_(entrada)/codigo.py
@@ -2,6 +2,11 @@ nome = input("Digite o seu nome: ")
 print(f"Bem vindo {nome}!")
 print(type(nome))
 
-numero = int(input("Olá! Informe um número: "))
-print(f"O número que você digitou foi: {numero}")
+numero = int(input("Olá! Informe um número inteiro: "))
+print(f"O número inteiro que você digitou menos 1 é: {numero - 1}")
 print(type(numero))
+
+
+numero_decimal = float(input("Agora informe um número decimal: "))
+print(type(numero_decimal))
+print(f"O número decimal que você digitou é: {numero_decimal}")
