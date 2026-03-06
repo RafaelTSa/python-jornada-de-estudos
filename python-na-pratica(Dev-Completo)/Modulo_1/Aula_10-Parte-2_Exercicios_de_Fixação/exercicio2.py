08828f8e9d15b84129d4d5873a3ea240fc80c8d7
Rafael T Santos <texera09@gmail.com>
2026-03-06 08:48:10 -0300

    adiciona exercicio2.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_10-Parte-2_Exercicios_de_Fixação/exercicio2.py
@@ -0,0 +1,5 @@
+numero1 = int(input("Digite um número inteiro: "))
+numero2 = int(input("Digite outro número inteiro: "))
+soma = numero1 + numero2
+
+print(f"O resultado da soma dos números é: {soma}")
