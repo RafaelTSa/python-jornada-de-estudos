27ff5fb7015cf4bddd1e90c8ffd54988675e7df7
Rafael T Santos <texera09@gmail.com>
2026-03-06 09:30:20 -0300

    adiciona exercicio3.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_10-Parte-2_Exercicios_de_Fixação/exercicio3.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media_nota = (nota1 + nota2) / 2
+media_round = round(media_nota, 1)
+
+print(f"A média das notas é: {media_round}")
