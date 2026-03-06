6f3868ffff6cb4dd18974bb7d031d824b4055a09
Rafael T Santos <texera09@gmail.com>
2026-03-06 11:31:36 -0300

    adiciona exercicio4.py

---
+++ python-na-pratica(Dev-Completo)/Modulo_1/Aula_10-Parte-2_Exercicios_de_Fixação/exercicio4.py
@@ -0,0 +1,5 @@
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+estado = input("Digite o estado em que você mora: ")
+
+print(f"{nome} tem {idade} anos e mora no estado {estado}.")
